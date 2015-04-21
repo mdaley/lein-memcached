8cab742d9641169272b373aa231c2f59e52adf4f
Matthew Daley <matt.daley@blueyonder.co.uk>
2015-04-21 09:04:51 +0100

    Bumped to 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-memcached "0.1.0-SNAPSHOT"
+(defproject lein-memcached "0.1.1-SNAPSHOT"
   :description "Creates an in-memory memcached server based on jmemcached"
   :url "https://github.com/mdaley/lein-memcached"
   :license {:name "Eclipse Public License"
